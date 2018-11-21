285ee3da34d490babaae1823b024d2c6fcc6b51d
Nicholas Matthew Morera <morera.computer@gmail.com>
2018-11-21 15:54:46 -0800

    basic rough index listing styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,37 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
-// import styled from 'styled-components'
+import styled from 'styled-components'
 import Layout from '../components/layout'
+
+const mono = `"Inconsolata", "Lucida Console", Monaco, monospace`
+const sansSerif = `'Karla', "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif`
+
+const PostCount = styled.p`
+  font-family: ${mono};
+  font-size: 0.8rem;
+`
+const PostTitle = styled.h3`
+  font-family: ${sansSerif};
+  font-size: 1.1rem;
+  color: black;
+  margin-bottom: -0.2rem;
+`
 
 export default ({ data }) => {
   return (
     <Layout>
-      <p>{data.allMarkdownRemark.totalCount} Posts</p>
+      <PostCount>{data.allMarkdownRemark.totalCount} Posts</PostCount>
       <hr />
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
-          <Link to={node.fields.slug}>
-            <h3>
+          <Link
+            to={node.fields.slug}
+            style={{ textDecoration: `none` }}
+          >
+            <PostTitle>
               {node.frontmatter.title}{' '}
-              <span>- {node.frontmatter.date}</span>
-            </h3>
+              <span>- {node.frontmatter.date.toLowerCase()}</span>
+            </PostTitle>
           </Link>
         </div>
       ))}
@@ -36,7 +53,7 @@ export const query = graphql`
           id
           frontmatter {
             title
-            date(formatString: "DD MMMM, YYYY")
+            date(formatString: "MMM DD, YYYY")
           }
           fields {
             slug
