d3d8786f57452a35c0848184d20e9ba09a192400
Nicholas Matthew Morera <morera.computer@gmail.com>
2018-11-21 02:52:16 -0800

    add styled-components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
-import React from "react"
+import React from 'react'
+import styled from 'styled-components'
 
-export default () => <div>Hello world!</div>
+const Heading = styled.h1`
+  color: blue;
+`
+
+export default () => (
+  <div>
+    <Heading>morera.computer</Heading>
+  </div>
+)
