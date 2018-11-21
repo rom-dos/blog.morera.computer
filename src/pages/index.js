1b1a48947a93e62f37efc40e771dcb4dd7ef5558
Nicholas Matthew Morera <morera.computer@gmail.com>
2018-11-21 03:01:25 -0800

    add basic initial index styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 650px;
+`
+
 const Heading = styled.h1`
   color: blue;
+  font-family: Helvetica;
+  font-weight: 400;
+  margin: 5rem auto 6rem auto;
+  text-align: center;
 `
 
 export default () => (
-  <div>
-    <Heading>morera.computer</Heading>
-  </div>
+  <Container>
+    <Heading>blog.morera.computer</Heading>
+    <hr />
+  </Container>
 )
